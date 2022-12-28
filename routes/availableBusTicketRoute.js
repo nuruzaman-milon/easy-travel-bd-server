a7f50af7b7ecc52cb9be23f02c6fc94daf92cff7
Njmilon <njmilon1@gmail.com>
2022-12-28 21:23:25 +0600

    available bus ticket route added

--- routes/availableBusTicketRoute.js
+++ routes/availableBusTicketRoute.js
@@ -1,3 +1,4 @@
+const { format } = require('date-fns');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -7,7 +8,7 @@ const AvailableBus = new mongoose.model('AvailableBus', availableBusTicketSchema
 
 // get all availble bus
 //http://localhost:5000/available-bus
-router.get('/', async(req,res)=>{
+router.get('/', async (req, res) => {
     try {
         const allavailableBus = await AvailableBus.find();
         if (allavailableBus) {
@@ -17,7 +18,7 @@ router.get('/', async(req,res)=>{
                 data: allavailableBus
             })
         }
-        else{
+        else {
             res.status(404).send({
                 success: false,
                 message: "bus not found",
@@ -33,9 +34,9 @@ router.get('/', async(req,res)=>{
 });
 // get all from & to location 
 //http://localhost:5000/available-bus/from-and-to
-router.get('/from-and-to', async(req,res)=>{
+router.get('/from-and-to', async (req, res) => {
     try {
-        const allavailablelocation = await AvailableBus.find().select({From:1, To:1});
+        const allavailablelocation = await AvailableBus.find().select({ From: 1, To: 1 });
         if (allavailablelocation) {
             res.status(200).send({
                 success: true,
@@ -43,10 +44,41 @@ router.get('/from-and-to', async(req,res)=>{
                 data: allavailablelocation
             })
         }
-        else{
+        else {
             res.status(404).send({
                 success: false,
                 message: "location not found",
+            })
+        }
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "server error occured",
+        })
+        console.log(error);
+    }
+});
+
+//query search data
+router.get('/search-data', async (req, res) => {
+    try {
+        const date = req.query.date;
+        const from = req.query.from;
+        const to = req.query.to;
+        const coachType = req.query.coachType;
+        const allavailablesearchBus = await AvailableBus.find({$and: [{Date: new Date(date)},{From:from},{To:to},{CoachType:coachType}]});
+
+        if (allavailablesearchBus) {
+            res.status(200).send({
+                success: true,
+                message: "bus found successfully",
+                data: allavailablesearchBus
+            })
+        }
+        else {
+            res.status(404).send({
+                success: false,
+                message: "bus not found",
             })
         }
     } catch (error) {
